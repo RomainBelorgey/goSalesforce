0fd14ff6e61be268f96b35bf5b50afbc8f215a50
Romain BELORGEY <romain.belorgey@gmail.com>
2018-01-04 15:53:42 +0100

    initial commit / need to add examples

---
+++ auth.go
@@ -0,0 +1,68 @@
+package goSalesforce
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Envelope struct {
+	XmlResponse XmlResponse `xml:"Body"`
+}
+type XmlResponse struct {
+	LoginResponse XmlResult `xml:"loginResponse"`
+}
+type XmlResult struct {
+	Result XmlAnswer `xml:"result"`
+}
+type XmlAnswer struct {
+	SessionId string `xml:"sessionId"`
+}
+
+// Will return a Oauth SessionID
+// The SessionId will be use for queries
+func SfAuth(url string, login string, password string, token string) string {
+
+	loginXml := fmt.Sprintf("<?xml version=\"1.0\" encoding=\"utf-8\" ?>"+
+		"<env:Envelope xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:partner.soap.sforce.com\">"+
+		"  <env:Header>"+
+		"    <urn:CallOptions>"+
+		"      <urn:client>RestForce</urn:client>"+
+		"      <urn:defaultNamespace>sf</urn:defaultNamespace>"+
+		"    </urn:CallOptions>"+
+		"  </env:Header>"+
+		"  <env:Body>"+
+		"    <n1:login xmlns:n1=\"urn:partner.soap.sforce.com\">"+
+		"      <n1:username>%s</n1:username>"+
+		"      <n1:password>%s</n1:password>"+
+		"    </n1:login>"+
+		"  </env:Body>"+
+		"</env:Envelope>", login, password+token)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url+"/services/Soap/u/41.0", bytes.NewReader([]byte(loginXml)))
+	if err != nil {
+		os.Exit(1)
+	}
+	req.Header.Add("SOAPAction", "login")
+	req.Header.Add("Content-Type", "text/xml; charset=UTF-8")
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	v := Envelope{}
+	err = xml.Unmarshal(contents, &v)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	return v.XmlResponse.LoginResponse.Result.SessionId
+}
---
+++ query.go
@@ -0,0 +1,37 @@
+package goSalesforce
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/ghodss/yaml"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+// Do a query to salesforce
+// Need the URL and the sessionId from Sfauth
+// I will return a yqnl content
+func SfQuery(urlSf string, sessionId string, query string) []byte {
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", urlSf+"/services/data/v41.0/query/", bytes.NewReader(nil))
+	if err != nil {
+		os.Exit(1)
+	}
+	q := req.URL.Query()
+	q.Add("q", query)
+	req.URL.RawQuery = q.Encode()
+	req.Header.Add("Authorization", "OAuth "+sessionId)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	y, err := yaml.JSONToYAML(contents)
+	return y
+
+}
